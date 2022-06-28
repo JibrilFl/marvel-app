2bdf2d6f3cc526bfa907743ec9527193861a15d4
AlexandrM <jibrilfl@gmail.com>
2022-06-29 00:16:49 +0500

    add api marvel

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,50 +1,73 @@
-import thor from '../../resources/thor.png';
+import { Component } from 'react';
+import MarvelService from '../../services/MarvelService';
+
 import mjolnir from '../../resources/shield-and-mjolnir.png';
 
 import './randomChar.scss';
 
-const RandomChar = () => {
-    return (
-        <div className="container">
-            <div className="randomChar">
-                <div className="randomChar__info">
-                    <img className="randomChar__info_img" src={thor} alt="thor" />
-                    <div className="randomChar__info_inner">
-                        <div className="randomChar__info_name">THOR</div>
-                        <p className="randomChar__info_descr">
-                            As the Norse God of thunder and lightning, Thor wields
-                            one of the greatest weapons ever made, the enchanted hammer Mjolnir.
-                            While others have described Thor as an over-muscled, oafish imbecile,
-                            he's quite smart and compassionate...
-                        </p>
-                        <div className="randomChar__info_btns">
-                            <a className="btn btn__main" href="#Homepage">
-                                <div className="inner">Homepage</div>
-                            </a>
-                            <a className="btn btn__secondary" href="#Wiki">
-                                <div className="inner">Wiki</div>
-                            </a>
+class RandomChar extends Component {
+    constructor(props) {
+        super(props);
+        this.updateChar();
+    }
+
+    state = {
+        char: {}
+    }
+
+    marvelService = new MarvelService();
+
+    onCharLoaded = (char) => {
+        this.setState({ char });
+    }
+
+    updateChar = () => {
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.marvelService
+            .getCharacter(id)
+            .then(this.onCharLoaded)
+    }
+
+    render() {
+        const { char: { name, description, thumbnail, homepage, wiki } } = this.state;
+
+        return (
+            <div className="container">
+                <div className="randomChar">
+                    <div className="randomChar__info">
+                        <img className="randomChar__info_img" src={thumbnail} alt="thor" />
+                        <div className="randomChar__info_inner">
+                            <div className="randomChar__info_name">{name}</div>
+                            <p className="randomChar__info_descr">{description}</p>
+                            <div className="randomChar__info_btns">
+                                <a className="btn btn__main" href={homepage}>
+                                    <div className="inner">Homepage</div>
+                                </a>
+                                <a className="btn btn__secondary" href={wiki}>
+                                    <div className="inner">Wiki</div>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="randomChar__box">
-                    <h2 className="randomChar__box_title">
-                        Random character for today! <br />
-                        Do you want to get to know him better?
-                    </h2>
-                    <p className="randomChar__box_another">
-                        Or choose another one
-                    </p>
-                    <div className="randomChar__box_btn">
-                        <a className="btn btn__main" href="#Homepage">
-                            <div className="inner">Try it</div>
-                        </a>
+                    <div className="randomChar__box">
+                        <h2 className="randomChar__box_title">
+                            Random character for today! <br />
+                            Do you want to get to know him better?
+                        </h2>
+                        <p className="randomChar__box_another">
+                            Or choose another one
+                        </p>
+                        <div className="randomChar__box_btn">
+                            <a className="btn btn__main" href="#Homepage">
+                                <div className="inner">Try it</div>
+                            </a>
+                        </div>
+                        <img className="randomChar__box_img" src={mjolnir} alt="shield and mjolnir" />
                     </div>
-                    <img className="randomChar__box_img" src={mjolnir} alt="shield and mjolnir" />
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default RandomChar;
---
+++ src/services/MarvelService.js
@@ -0,0 +1,49 @@
+class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=a29ee460fa7b8754a4ffa13551bb6eb6';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    editDescr = (descr, limit) => {
+        if (descr) {
+            if (descr.length >= limit) {
+                return descr.slice(0, limit) + '...';
+            } else {
+                return descr;
+            }
+        } else {
+            return 'Description not found';
+        }
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: this.editDescr(char.description, 200),
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
