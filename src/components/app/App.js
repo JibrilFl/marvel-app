9079c3a2da6570a5db4da52e72f224bba80f1499
AlexandrM <jibrilfl@gmail.com>
2022-07-09 23:25:03 +0500

    merge

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppHeader from '../appHeader/AppHeader';
+import AppBanner from '../appBanner/AppBanner';
 import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
@@ -17,24 +19,33 @@ const App = () => {
 	}
 
 	return (
-		<div className='app'>
-			<AppHeader />
-			<main>
-				{/* <ErrorBoundary>
-					<RandomChar />
-				</ErrorBoundary>
-				<div className="charList__wrapper">
-					<ErrorBoundary>
-						<CharList onCharSelected={onCharSelected} />
-					</ErrorBoundary>
-					<ErrorBoundary>
-						<CharInfo charId={selectedChar} />
-					</ErrorBoundary>
-				</div>
-				<img className='bg-decoration' src={decoration} alt="vigen" /> */}
-				<ComicList />
-			</main>
-		</div>
+		<Router>
+			<div className='app'>
+				<AppHeader />
+				<main>
+					<Switch>
+						<Route exact path="/">
+							<ErrorBoundary>
+								<RandomChar />
+							</ErrorBoundary>
+							<div className="charList__wrapper">
+								<ErrorBoundary>
+									<CharList onCharSelected={onCharSelected} />
+								</ErrorBoundary>
+								<ErrorBoundary>
+									<CharInfo charId={selectedChar} />
+								</ErrorBoundary>
+							</div>
+							<img className='bg-decoration' src={decoration} alt="vigen" />
+						</Route>
+						<Route exact path="/comics">
+							<AppBanner />
+							<ComicList />
+						</Route>
+					</Switch>
+				</main>
+			</div>
+		</Router>
 	);
 }
 
