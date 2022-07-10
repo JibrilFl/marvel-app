414feab9de16da886ab206543ec5dc86c9774c9e
AlexandrM <jibrilfl@gmail.com>
2022-07-10 23:50:04 +0500

    route v6

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,54 +1,21 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppHeader from '../appHeader/AppHeader';
-import AppBanner from '../appBanner/AppBanner';
-import RandomChar from '../randomChar/RandomChar';
-import CharList from '../charList/CharList';
-import CharInfo from '../charInfo/CharInfo';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
-import ComicList from '../comicList/ComicList';
+import { MainPage, ComicsPage } from '../pages';
 
-import decoration from '../../resources/vigen-bg.png';
 
 const App = () => {
 
-	const [selectedChar, setChar] = useState(null);
-	const [selectedComic, setComic] = useState(null);
 
-	const onCharSelected = (id) => {
-		setChar(id);
-	}
-
-	const onComicSelected = (id) => {
-		setComic(id);
-		console.log('Гача!');
-	}
 
 	return (
 		<Router>
 			<div className='app'>
 				<AppHeader />
 				<main>
-					<Switch>
-						<Route exact path="/">
-							<ErrorBoundary>
-								<RandomChar />
-							</ErrorBoundary>
-							<div className="charList__wrapper">
-								<ErrorBoundary>
-									<CharList onCharSelected={onCharSelected} />
-								</ErrorBoundary>
-								<ErrorBoundary>
-									<CharInfo charId={selectedChar} />
-								</ErrorBoundary>
-							</div>
-							<img className='bg-decoration' src={decoration} alt="vigen" />
-						</Route>
-						<Route exact path="/comics">
-							<AppBanner />
-							<ComicList />
-						</Route>
-					</Switch>
+					<Routes>
+						<Route path="/" element={<MainPage />} />
+						<Route path="/comics" element={<ComicsPage />} />
+					</Routes>
 				</main>
 			</div>
 		</Router>
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,16 +1,23 @@
+import { Link, NavLink } from 'react-router-dom';
 import './appHeader.scss';
 
 const AppHeader = () => {
     return (
         <div className="container">
             <div className="appHeader">
-                <h1 className='appHeader__title'><span>Marvel</span> information portal</h1>
+                <h1 className='appHeader__title'><Link to="/"><span>Marvel</span> information portal</Link></h1>
                 <ul className="nav">
                     <li className="nav__list">
-                        <a className="nav__list-link active" href="Characters">Characters</a>
+                        <NavLink
+                            className={({ isActive }) => "nav__list-link" + (isActive ? " active" : "")}
+                            to="/"
+                            end>Characters</NavLink>
                     </li>
                     <li className="nav__list">
-                        <a className="nav__list-link" href="Comics">Comics</a>
+                        <NavLink
+                            className={({ isActive }) => "nav__list-link" + (isActive ? " active" : "")}
+                            to="/comics"
+                            end>Comics</NavLink>
                     </li>
                 </ul>
             </div>
